292b9c1b0a36432e7ca44c046ff977f82331f1e2
Klight96 <klightkayode05@gmail.com>
2023-10-26 20:07:15 +0100

    update base url

--- server/controllers/resdCntrl.js
+++ server/controllers/resdCntrl.js
@@ -330,41 +330,41 @@ export const createResidency = asyncHandler(async (req, res) => {
     }
   });
 
-  // const {
-  //   title,
-  //   description,
-  //   price,
-  //   address,
-  //   country,
-  //   city,
-  //   facilities,
-  //   image,
-  //   userEmail,
-  // } = req.body.data;
+  const {
+    title,
+    description,
+    price,
+    address,
+    country,
+    city,
+    facilities,
+    image,
+    userEmail,
+  } = req.body.data;
 
-  // console.log(req.body.data);
-  // try {
-  //   const residency = await prisma.residency.create({
-  //     data: {
-  //       title,
-  //       description,
-  //       price,
-  //       address,
-  //       country,
-  //       city,
-  //       facilities,
-  //       image,
-  //       owner: { connect: { email: userEmail } },
-  //     },
-  //   });
+  console.log(req.body.data);
+  try {
+    const residency = await prisma.residency.create({
+      data: {
+        title,
+        description,
+        price,
+        address,
+        country,
+        city,
+        facilities,
+        image,
+        owner: { connect: { email: userEmail } },
+      },
+    });
 
-  //   res.send({ message: "Residency created successfully", residency });
-  // } catch (err) {
-  //   if (err.code === "P2002") {
-  //     throw new Error("A residency with address already there");
-  //   }
-  //   throw new Error(err.message);
-  // }
+    res.send({ message: "Residency created successfully", residency });
+  } catch (err) {
+    if (err.code === "P2002") {
+      throw new Error("A residency with address already there");
+    }
+    throw new Error(err.message);
+  }
 });
 
 // function to get all the documents/residencies
