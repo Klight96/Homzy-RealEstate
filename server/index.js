6e7a6a6483345d65f7916ddc7f09ef103192693a
Klight96 <klightkayode05@gmail.com>
2023-10-27 01:31:13 +0100

    add cors

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ import { userRoute } from './routes/userRoute.js';
 import { residencyRoute } from './routes/residencyRoute.js';
 dotenv.config()
 
+const express = require('express');
+const cors = require('cors');
+
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
